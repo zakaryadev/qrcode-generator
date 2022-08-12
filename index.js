9a43c8662b1009b3db79e437c1e335327760015b
Zakarya <zakaryamuratbaev13@gmail.com>
2022-08-13 00:40:45 +0500

    A

--- index.js
+++ index.js
@@ -7,15 +7,15 @@ let preValue;
 
 generateBtn.addEventListener("click", () => {
   let qrValue = qrInput.value.trim();
-  if(qrValue != "") {
-  preValue = qrValue;
-  generateBtn.innerText = "QR Code Generaciyalanbaqta...";
-  qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=${qrValue}`;
-  qrImg.addEventListener("load", (e) => {
-    generateBtn.innerText = "QR Code Generaciyalaw";
-    wrapper.classList.add("active");
-  });
-}
+  if (qrValue != "") {
+    preValue = qrValue;
+    generateBtn.innerText = "QR Code Generaciyalanbaqta...";
+    qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=${qrValue}&bgcolor=fff&color=000`;
+    qrImg.addEventListener("load", (e) => {
+      generateBtn.innerText = "QR Code Generaciyalaw";
+      wrapper.classList.add("active");
+    });
+  }
 });
 
 qrInput.addEventListener("keyup", () => {
